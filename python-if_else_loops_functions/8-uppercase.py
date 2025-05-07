96c23e74636d475f807c50d604c8daf99cce96ff
xl-c111 <10430@holbertonstudents.com>
2025-05-08 01:34:25 +1000

    prints a string in uppercase followed by a new line

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    uppercase_str = ""
+    for i in str:
+        if 97 <= ord(i) <= 122:
+            uppercase_str += chr(ord(i) - 32)
+        else:
+            uppercase_str += i
+    print(uppercase_str)
