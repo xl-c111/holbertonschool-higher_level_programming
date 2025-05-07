63b17fc5d4c76749a903ca0a8611d4bea9b4a902
xl-c111 <10430@holbertonstudents.com>
2025-05-08 02:52:35 +1000

    add comments

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+# in range(n) already give numbers in decimal
 for i in range(0, 99):
+    # f"{i:x}" converts decimal into lowercase hexadecimal
+    # f"{i:X}" converts decimal into uppercase hexadecimal
+    # f"{i:#x}" converts decimal into a lowercase hexadecimal with 0x prefix
+    # f"{i:#X}" converts decimal into a uppercase hexadecimal with 0x prefix
+    # f"{i:02x}" converts decimal into a lowercase hexadecimal, zero-padded 2 digits
     print("{} = {:#x}".format(i, i))
