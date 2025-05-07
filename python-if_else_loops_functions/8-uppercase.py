9afc128eaa9d608020b0488d678916723d744c35
xl-c111 <10430@holbertonstudents.com>
2025-05-08 01:41:19 +1000

    fix .format() error

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    uppercase_str = ""
-    for i in str:
-        if 97 <= ord(i) <= 122:
-            uppercase_str += chr(ord(i) - 32)
-        else:
-            uppercase_str += i
-    print(uppercase_str)
+    for c in str:
+        if 97 <= ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print()
