5b1d25d7d7c3fdc13774a506eb4917c48ee0696b
xl-c111 <10430@holbertonstudents.com>
2025-05-11 01:27:15 +1000

    fix __main__ pattern

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
-arg_count = len(argv) - 1
-if arg_count == 0:
-    print("0 arguments.")
-else:
-    if arg_count == 1:
-        print("{} argument: ".format(arg_count))
+if __name__ == "__main__":
+    arg_count = len(argv) - 1
+    if arg_count == 0:
+        print("0 arguments.")
     else:
-        print("{} arguments: ".format(arg_count))
-    for i in range(1, len(argv)):
-        print("{}: {}".format(i, argv[i]))
+        if arg_count == 1:
+            print("{} argument: ".format(arg_count))
+        else:
+            print("{} arguments: ".format(arg_count))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
