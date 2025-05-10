c1cb59fa9e28995e23debbf951f545f9f23aed61
xl-c111 <10430@holbertonstudents.com>
2025-05-11 01:21:10 +1000

    prints the number of and the list of its arguments.

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+arg_count = len(argv) - 1
+if arg_count == 0:
+    print("0 arguments.")
+else:
+    if arg_count == 1:
+        print("{} argument: ".format(arg_count))
+    else:
+        print("{} arguments: ".format(arg_count))
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
