0e671ebd47733a295c0a93ac394947bf24571e94
xl-c111 <10430@holbertonstudents.com>
2025-05-11 05:18:15 +1000

    add comments

--- python-import_modules/easy_print.py
+++ python-import_modules/easy_print.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""
+import os to import built-in os mudule, which provides a way to interact with operating system 
+os.write(): send output directly to the terminal
+os.write(fd, data): argument 1 - file descriptor for standard output (stdout)
+                                 0 = standard input
+                                 1 = standard output
+                                 2 = standard error
+                    argument 2: b"#pythoniscool\n"
+                                prefix b: means this is a bytes literal
+                                "#pythoniscool\n" is the message followed by a newline character
+"""
 import os
 os.write(1, b"#pythoniscool\n")
