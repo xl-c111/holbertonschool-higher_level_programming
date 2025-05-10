5a6020c7019009446d934275417a2ae0dd857a25
xl-c111 <10430@holbertonstudents.com>
2025-05-11 04:21:11 +1000

    fix error - You are not allowed to use * for importing or __import__

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
-from calculator_1 import add, sub, mul, div
+import calculator_1
 
 if __name__ == "__main__":
 
@@ -17,10 +17,10 @@ if __name__ == "__main__":
         a = int(sys.argv[1])
         b = int(sys.argv[3])
         if operator == "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
+            print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
         elif operator == "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
+            print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
         elif operator == "*":
-            print("{} * {} = {}".format(a, b, mul(a, b)))
+            print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
         elif operator == "/":
-            print("{} / {} = {}".format(a, b, div(a, b)))
+            print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
