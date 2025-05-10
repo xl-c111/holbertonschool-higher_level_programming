7c6053be87d15670e416fb55cb1ff8d990bc6d4e
xl-c111 <10430@holbertonstudents.com>
2025-05-11 05:33:53 +1000

    testing

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,13 +2,8 @@
 import sys
 import calculator_1
 
-if __name__ == "__main__":
 
-    arg_count = len(sys.argv) - 1
-    if arg_count != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-
+def my_calculator():
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
@@ -25,3 +20,15 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     print("{} {} {} = {}".format(a, operator, b, result))
+
+
+def main():
+    arg_count = len(sys.argv) - 1
+    if arg_count != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    my_calculator()
+
+
+if __name__ == "__main__":
+    main()
