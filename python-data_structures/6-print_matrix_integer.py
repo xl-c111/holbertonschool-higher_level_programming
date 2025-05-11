673ec889cb63700042323bb09e1b788743b0eff8
xl-c111 <10430@holbertonstudents.com>
2025-05-12 02:17:09 +1000

    prints a matrix of integers.

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for j in range(len(row)):
+            # check if j is the last element of each row
+            if j < len(row) - 1:
+                print("{}".format(row[j]), end=" ")
+            else:
+                print("{}".format(row[j]), end="")
+        print()
