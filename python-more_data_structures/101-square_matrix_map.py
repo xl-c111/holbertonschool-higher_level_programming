9d66cc2d6c16acfa7977cbc6e4ffe719737f8606
xl-c111 <10430@holbertonstudents.com>
2025-05-13 02:56:08 +1000

    for is not allowed

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
     return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
-
-
-# Another solution by using map and for
-"""
-return [list(map(lambda x: x ** 2, row)) for row in matrix]
-"""
