7e20a27f158237e79acc46923dfc11aa4b036e0a
xl-c111 <10430@holbertonstudents.com>
2025-05-12 12:02:16 +1000

    add comments

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -16,3 +16,21 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     result = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
     return result
+
+
+# Another way to add two tuples - 1
+"""
+result = []
+for i in range(len(tuple_a)):
+    result.append(tuple_a[i] + tuple_b[i])
+result = tuple(result)
+return result
+"""
+# Another way to add two tuples - 2
+""" 
+result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+"""
+# Another way to add two tuples - 3
+"""
+result = tuple(map(sum, zip(tuple_a, tuple_b)))
+"""
--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,3 +4,9 @@ def multiple_returns(sentence):
         return (0, None)
     else:
         return (len(sentence), sentence[0])
+
+
+# Another way to check if str is empty
+"""
+if len(str) == 0:
+"""
