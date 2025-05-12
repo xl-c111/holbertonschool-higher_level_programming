a043e2445f57d641361938c9a56cc1284575d388
xl-c111 <10430@holbertonstudents.com>
2025-05-12 23:08:22 +1000

    fix output length

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,8 +4,6 @@ def best_score(a_dictionary):
         return None
     else:
         max_val = max(a_dictionary.values())
-        max_keys = []
         for key in a_dictionary:
             if a_dictionary[key] == max_val:
-                max_keys.append(key)
-        return max_keys
+                return key
