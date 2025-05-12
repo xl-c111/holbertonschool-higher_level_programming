0045c5b6ea50df332f3249b634897f035f1804b8
xl-c111 <10430@holbertonstudents.com>
2025-05-12 11:41:56 +1000

    adds 2 tuples

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,3 +2,9 @@
 def print_list_integer(my_list=[]):
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
+
+
+# Another solution
+# def print_list_integer(my_list=[]):
+#     for i in my_list:
+#         print("{:d}".format(i))
