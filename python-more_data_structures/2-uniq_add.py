7becbd3fa5012de50daa80d6d90c7c03cb1c5d84
xl-c111 <10430@holbertonstudents.com>
2025-05-12 21:15:52 +1000

    add comments

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -4,3 +4,26 @@ def uniq_add(my_list=[]):
     for i in set(my_list):
         sum += i
     return sum
+
+
+# Another way to get a uniq list - 1
+'''
+def uniq_add(my_list=[]):
+    uniq_list = []
+    for item in my_list:
+        if item not in uniq_list:
+            uniq_list.append(item)
+'''
+
+# Another way to get a uniq list - 1
+'''
+from collections import OrderedDict
+
+
+def uniq_add(my_list=[]):
+    uniq_list = list(OrderedDict.fromkeys(my_list))
+    sum = 0
+    for i in uniq_list:
+        sum += i
+    return sum
+'''
