29c4bfe41b7f868e5bc576b633260a2a0113d62e
xl-c111 <10430@holbertonstudents.com>
2025-05-12 18:59:13 +1000

    add comments

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for row in matrix:
-        new_row = []
-        for element in row:
-            new_row.append(element ** 2)
-        new_matrix.append(new_row)
+
+
+new_matrix = []
+for row in matrix:
+    new_row = []
+    for element in row:
+        new_row.append(element ** 2)
+    new_matrix.append(new_row)
+return new_matrix
+
+
+# Another way to compute the square value of all integers of a matrix
+
+# for row in matrix: outer list comprehension -> iterate over each row
+# for element in row: inner list comprehensionz -> iterate over each element in the current row
+# combined logic: outer loop builds new row, inner loop squares each element in that row
+
+"""
+def square_matrix_simple(matrix=[]):
+    new_matrix = [[element**2 for element in row] for row in matrix]
     return new_matrix
+"""
