309835a2c566bf23ba8031fa1328ae8c20a10688
xl-c111 <10430@holbertonstudents.com>
2025-05-12 23:57:14 +1000

    add comments

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -7,14 +7,18 @@ def best_score(a_dictionary):
         return max_key
 
 
-# Another way to get max value - 1
+# Another way to get the key with max value - Using a loop
 '''
 max_val = max(a_dictionary.values())
     for key in a_dictionary:
-        if a_dictionary[key] == max_val: 
+        if a_dictionary[key] == max_val:
             return key
 '''
-# Another way to get max value - 2
+# Another way to get the key with max value - Using lambda function
 '''
- max_key = max(a_dictionary, key=lambda key: a_dictionary[key])
+max_key = max(a_dictionary, key=lambda key: a_dictionary[key])
+'''
+# Another way to get the key with max value - Using sorted()
+'''
+max_key = sorted(a_dictionary, key=a_dictionary.get, reverse=True)[0]
 '''
