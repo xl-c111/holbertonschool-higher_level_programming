eb371221eb24af01f1d00e52925e4c9a9f7d6ebc
xl-c111 <10430@holbertonstudents.com>
2025-05-12 23:47:20 +1000

    add comments

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,18 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     else:
-        max_val = max(a_dictionary.values())
-        for key in a_dictionary:
-            if a_dictionary[key] == max_val:
-                return key
+        max_key = max(a_dictionary, key=a_dictionary.get)
+        return max_key
+
+
+# Another way to get max value - 1
+'''
+max_val = max(a_dictionary.values())
+    for key in a_dictionary:
+        if a_dictionary[key] == max_val: 
+            return key
+'''
+# Another way to get max value - 2
+'''
+ max_key = max(a_dictionary, key=lambda key: a_dictionary[key])
+'''
