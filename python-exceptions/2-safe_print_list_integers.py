cc416def222df2f80abc4e59c8eac53b80bca00e
xl-c111 <10430@holbertonstudents.com>
2025-05-13 21:12:52 +1000

    add comments

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,9 +5,11 @@ def safe_print_list_integers(my_list=[], x=0):
     while i < x:
         try:
             print("{:d}".format(my_list[i]), end="")
+            # increase count only when my_list[i] printed successfully as an int
             count += 1
         except (TypeError, ValueError):
             pass
+        # increase i whether the current element is int or not -> ensure access first x element, print only integers
         i += 1
     print()
     return count
