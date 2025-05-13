b32db601a64c30d6c4539e189809b3b10943acf5
xl-c111 <10430@holbertonstudents.com>
2025-05-13 23:39:32 +1000

    add comments

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,6 +2,8 @@
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
+        # try inside for loop, each element is tries independently
+        # try outside for loop, loop stops after first error
         try:
             result = my_list_1[i] / my_list_2[i]
         except TypeError:
@@ -16,3 +18,27 @@ def list_division(my_list_1, my_list_2, list_length):
         finally:
             new_list.append(result)
     return new_list
+
+
+# Another solution - Using while loop
+'''
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    i = 0
+    while i < list_length:
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            result = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        finally:
+            new_list.append(result)
+        i += 1
+    return new_list
+'''
