c47247f7c84c45aa652353451fd3e8bf98b79a6d
xl-c111 <10430@holbertonstudents.com>
2025-05-13 11:45:34 +1000

    converts a Roman numeral to an integer

--- python-more_data_structures/12-main.py
+++ python-more_data_structures/12-main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Roman to Integer test file
+""" 
+Roman to Integer test file
 """
 roman_to_int = __import__('12-roman_to_int').roman_to_int
 
--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
     else:
+        roman_dict = {'I': 1, 'V': 5, 'X': 10,
+                      'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+        total_value = 0
+        prev_value = 0
+
+        for char in reversed(roman_string):
+            current_value = roman_dict.get(char, 0)
+            if current_value < prev_value:
+                total_value -= current_value
+            else:
+                total_value += current_value
+                prev_value = current_value
+        return total_value
