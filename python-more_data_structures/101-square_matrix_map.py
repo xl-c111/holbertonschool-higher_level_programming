6c43de5e4a2b2f2973de0eef24b277d640f34ea1
xl-c111 <10430@holbertonstudents.com>
2025-05-13 10:49:54 +1000

    add comments

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/p:ython3
+
+# map(lambda row: ..., matrix): apply a function to each row
+# list(map(lambda x: x**2, row)): takes each element x in the row and returns its square (x**2), then wraps the result as a list
+
 def square_matrix_map(matrix=[]):
     return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
 
@@ -6,4 +10,14 @@ def square_matrix_map(matrix=[]):
 # Another solution by using map and for
 """
 return [list(map(lambda x: x ** 2, row)) for row in matrix]
+"""
+
+# Another solution by using NumPy
+"""
+import numpy as np
+
+
+np_matrix = np.array(matrix)
+squared_matrix = np_matrix ** 2
+return squared_matrix
 """
