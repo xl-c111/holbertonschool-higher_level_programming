c4be88988b3566fa63d3f4b926ce902b30dcd341
xl-c111 <10430@holbertonstudents.com>
2025-05-13 12:26:36 +1000

    add comments

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+
 def complex_delete(a_dictionary, value):
+    # key in a_dictionary to check key
+    # value in a_dictionary.values() to check value
+    # key, value in a_dictionary.items() to check key-value pair
     if value not in a_dictionary.values():
         return a_dictionary
     else:
@@ -20,4 +24,10 @@ def complex_delete(a_dictionary, value):
             del a_dictionary[k]
 
         return a_dictionary
+'''
+# Another way to delete keys with a specific value
+'''
+    for key in a_dictionary:
+        if a_dictionary[key] == Value:
+            del a_dictionary[key]
 '''
