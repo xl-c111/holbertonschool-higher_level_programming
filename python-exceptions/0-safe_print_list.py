cf2bfd5a613e9b632016f08be07d634cff2c3f1f
xl-c111 <10430@holbertonstudents.com>
2025-05-14 14:30:51 +1000

    add comments

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,8 +4,6 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     while i < x:
         try:
-            # print the element at index i, if i within x, succeeds.
-            # if i out of bound, this line will raise IndexError
             print("{}".format(my_list[i]), end="")
             # increase the count only when my_list[i] is actually printed
             count += 1
