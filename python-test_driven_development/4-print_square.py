bdd9fdc721418f2d4380be61f7825ca49e9501e7
xl-c111 <10430@holbertonstudents.com>
2025-05-17 23:37:16 +1000

    add module docstring, function docstring and test case: one argument missing

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
+"""
+This module provides a function to print a square of size 'size' using
+the '#' character.
+It checks if size is a non-negative integer. 
+It raises TypeError if size is not an integer. 
+It raises ValueError if size is negative. 
+
+"""
+
+
 def print_square(size):
+    """
+    Prints a square of size `size` using the '#' character.
+
+    Args:
+        size: is the size length of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is negative.
+
+    Returns:
+        None
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    if isinstance(size, float) or size < 0:
-        raise TypeError("size must be an integer")
 
     for i in range(size):
         print("#" * size)
