9b8d0ba99de7fd7f6ccd6b0410232707f0946de9
xl-c111 <10430@holbertonstudents.com>
2025-05-18 03:30:51 +1000

    add more test cases

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -9,11 +9,23 @@ class TestMaxInteger(unittest.TestCase):
     def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
 
-    def test_all_positive_elements_list(self):
+    def test_max_the_end(self):
         self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
 
-    def test_all_negative_elements_list(self):
+    def test_max_in_the_middle(self):
+        self.assertEqual(max_integer([1, 2, 5, 4, 3]), 5)
+
+    def test_max_the_beginning(self):
+        self.assertEqual(max_integer([3, 2, 1]), 3)
+
+    def test_one_negative_number(self):
+        self.assertEqual(max_integer([-1, 2, 8]), 8)
+
+    def test_only_negative_numbers(self):
         self.assertEqual(max_integer([-1, -5, -2]), -1)
+
+    def test_list_of_one_number(self):
+        self.assertEqual(max_integer([5]), 5)
 
     def test_wrong_max(self):
         self.assertNotEqual(max_integer([1, 2, 3]), 2)
