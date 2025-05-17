d7497acae21196fe335116074eebb4849e8f038d
xl-c111 <10430@holbertonstudents.com>
2025-05-18 01:46:05 +1000

    fix output

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,27 +3,17 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     temp = ""
-    for char in text:
-        temp += char
-        if char in ".?:":
+    i = 0
+    while i < len(text):
+        temp += text[i]
+        if text[i] in ".?:":
             print(temp.strip())
             print()
             temp = ""
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+        i += 1
     if temp.strip():
         print(temp.strip())
-
-    # i = 0
-    # temp = ""
-    # while i < len(text):
-    #     temp += text[i]
-    #     if text[i] == '.' or text[i] == '?' or text[i] == ':':
-    #         print(temp.strip())
-    #         print()
-    #         temp = ""
-    #         i += 1
-    #         while i < len(text) and text[i] == " ":
-    #             i += 1
-    #         continue
-    #     i += 1
-    # if temp.strip():
-    #     print(temp.strip())
