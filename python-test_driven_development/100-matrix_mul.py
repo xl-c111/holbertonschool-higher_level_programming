2a9fdaa5105395306f2855e8938512fae45f146c
xl-c111 <10430@holbertonstudents.com>
2025-05-18 04:00:00 +1000

    multiplies 2 matrices

---
+++ python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+def matrix_mul(m_a, m_b):
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list")
+    if m_a == []:
+        raise ValueError("m_a can't be empty")
+    if m_b == []:
+        raise ValueError("m_b can't be empty")
+    for row in m_a:
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("m_a should contain only integers or floats")
+    for row in m_b:
+        for element in row:
+            if not isinstance(element, (int, float)):
+                raise TypeError("m_a should contain only integers or floats")
+    for row in m_a:
+        if len(row) != len(m_a[0]):
+            raise TypeError("each row of m_a must be of the same size")
+    for row in m_b:
+        if len(row) != len(m_b[0]):
+            raise TypeError("each row of m_b must be of the same size")
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+    result = []
+    for i in range(len(m_a)):
+        row = []
+        for j in range(len(m_b[0])):
+            sum = 0
+            for k in range(len(m_b)):
+                sum += m_a[i][k] * m_b[k][j]
+            row.append(sum)
+        result.append(row)
+    return result
