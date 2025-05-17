e0dbe41eff8a6269e1f7676d4139d6f465914fbe
xl-c111 <10430@holbertonstudents.com>
2025-05-18 01:50:49 +1000

    add module docstring and function docstring

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python3
+"""
+This module provides a function to print a formatted text with
+two newlines after each occurrence of '.', '?' or ':'.
+
+It ensures that each printed line has no leading or trailing spaces.
+"""
+
+
 def text_indentation(text):
+    """
+    Prints a text with two newlines after each '.', '?' or ':' character.
+
+    The function removes any leading or trailing whitespace from each printed line.
+    It skips any spaces immediately following '.', '?' or ':' to prevent leading
+    spaces in the next printed segment.
+
+    Args:
+
+    Raises:
+        TypeError: If the input is not a string.
+
+    Returns:
+        None
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     temp = ""
