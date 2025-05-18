ba9312ec2d2806e535230b1296da934aa1cb152e
xl-c111 <10430@holbertonstudents.com>
2025-05-18 22:36:57 +1000

    update another solution

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -8,3 +8,22 @@ def print_matrix_integer(matrix=[[]]):
             else:
                 print("{:d}".format(row[j]), end="")
         print()
+
+
+# Another way to print a matrix
+"""
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 0:
+        return None
+
+    row = 0
+    while row < len(matrix):
+        i = 0
+        while i < len(matrix[0]):
+            if i != 0:
+                print(" ", end="")
+            print("{:d}".format(matrix[row][i]), end="")
+            i += 1
+        print()
+        row += 1
+"""
