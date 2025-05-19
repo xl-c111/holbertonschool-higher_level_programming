6038aee766e2da5e8fb19a7410cac04de9ad3aa2
xl-c111 <10430@holbertonstudents.com>
2025-05-19 16:28:31 +1000

    add mudule docstring and function docstring

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+"""
+This module provides a simple implementation of an empty Square Class.
+"""
+
+
 class Square:
+    """
+    Represents a square (no attributes or methods).
+
+    Methods
+    -------
+    __init__():
+        Initializes an instance of the Square class.
+    """
+
     def __init__(self):
+        """Initializes an empty Square instance."""
         pass
