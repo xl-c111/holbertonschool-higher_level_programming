42b93b8f5343ba5427e5b9b588f5b179826cf64c
xl-c111 <10430@holbertonstudents.com>
2025-05-20 01:23:10 +1000

    Print Square instance

---
+++ python-classes/101-square.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class that supports size and position,
+and can be displayed both by printing and as a string with custom spacing.
+"""
+
+
+class Square:
+    """
+    Represents a square with given size and position.
+
+    Attributes
+        __size: integer - the length of sides of square.
+        ————position: tuple - the (x, y) offset for printing
+
+    Methods
+        __init__(size=0): initializes a Square instance with a given size.
+        area(): return the area of the square.
+        my_print(): print the square to stdout with offset.
+
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Initializes a Square with given size.
+
+        Arguments
+            size : int - The length of the sides of the square(default is 0).
+            position: the (x, y) position offset for printing.
+
+        """
+        # Use self.size = size inside __init__ to trigger setter to validate
+        # and set value
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """
+        get the size of the square.
+        Returns
+            int: the size of the square.
+        """
+        # Use self.__size inside property methods and internal methods
+        # to access the stored actual value.
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        set the size of the square.
+
+        Parameters:
+            value: int - the size of the square.
+
+        Raises
+            TypeError: if size is not an integer.
+            ValueError: if size is less than 0.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    @property
+    def position(self):
+        """
+        get the current position of the square.
+
+        Returns
+            tuple of int: the position offset as a tuple(x, y).
+            x is the number of spaces to skip before each line.
+            y is the number of blank lines to skip before the square.
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """
+        set the positionz of the square.
+
+        Parameters:
+            value: tuple of two non-negative integers(x, y)
+                x specifies the horizontal space offset.
+                y specifies the number of blank lines above the square.
+
+        Raises:
+            TypeError: if the value is not a tuple of two non-negative ints
+
+        """
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(isinstance(num, int) and num >= 0 for num in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """
+        return the area of the square.
+
+        Returns:
+            int - the area of the square.
+        """
+
+        return self.__size * self.__size
+
+    def my_print(self):
+        """
+        prints in stdout the square with the character #.
+        If size is 0, prints an empty line.
+        The number of blank lines printed above the square is
+            determinded by position[1].
+        The number of spaces printed before each line is determinded by
+            position[0].
+        """
+        if self.__size == 0:
+            print()
+            # return ensures function stops immediately after printing an
+            # empty line when size == 0
+            return
+        for _ in range(self.position[1]):
+            # the underscore _ used as a variable name when loop variable
+            #        itself is not needed
+            # this loop repeats the intended code self.position[1] times.
+            print()
+        i = 0
+        while i < self.__size:
+            print("{}{}".format((' ' * self.position[0]), ('#' * self.__size)))
+            i += 1
+
+    def __str__(self):
+        """
+        returns a string that visually represents a square, with spaces and
+        blank lines determinded by position attribute.
+        The output consists of:
+            - position[1] empty line before square
+            - position[0] times spaces before each line of the square
+            - each line contains self.__size times '#'
+
+        Returns
+            str: The square as a formatted string.
+        """
+        if self.__size == 0:
+            return ""
+
+        result = []
+        for _ in range(self.position[1]):
+            result.append("")
+
+        for i in range(self.__size):
+            result.append("{}{}".format(
+                (' ' * self.position[0]), ('#' * self.__size)))
+        return "\n".join(result)
+        print(self.__str__())
