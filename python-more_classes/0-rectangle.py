fdc024afed02a9ab1d9d76b85abb51df9d6a6e93
xl-c111 <10430@holbertonstudents.com>
2025-05-20 10:49:51 +1000

    Simple rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Rectangle:
+    """
+    This module provides a simple implementation of an empty rectangle Class.
+    """
+
+    def __init__(self):
+        """
+    Represents a rectangle (no attributes or methods).
+
+    Methods
+        __init__(): initializes an instance of the rectangle class.
+    """
+
+        pass
