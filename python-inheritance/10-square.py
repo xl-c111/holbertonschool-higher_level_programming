c3aa784f1fe8d5b29c479c3a0276173cb5b3b218
xl-c111 <10430@holbertonstudents.com>
2025-05-29 15:16:54 +1000

    fix print(issubclass(Square, Rectangle)) output error

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -68,8 +68,6 @@ class Square(Rectangle):
         """
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size, size)
-        # Rectangle.__init__（self, size, size）
 
     def area(self):
         """
--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -68,7 +68,6 @@ class Square(Rectangle):
         """
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size, size)
 
     def area(self):
         """
