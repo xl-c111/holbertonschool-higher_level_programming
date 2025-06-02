663ff9111cc33e31033373cc7f273525a5cc820c
xl-c111 <10430@holbertonstudents.com>
2025-06-02 22:08:31 +1000

    add docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+This module provides a utility function to read and print the content of a file
+"""
+
+
 def read_file(filename=""):
+    """ Reads a text file (UTF-8) and prints its content to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
         content = f.read()
         print(content, end="")
