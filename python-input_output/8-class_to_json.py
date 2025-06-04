c6a2655e8e945cb5409f3d7f087afa0896495ad3
xl-c111 <10430@holbertonstudents.com>
2025-06-04 22:28:51 +1000

    fix error - import module is not allowed

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 """This module provides a function that converts class object into its
 JSON representation"""
-import json
 
 
 def class_to_json(obj):
-    """ Convert a class object to a JSON string."""
-    return json.dumps(obj.__dict__)
+    """
+    Returns a dictionary description of a class instance suitable for
+    JSON serialization.
+
+    Args:
+        obj: the class instance whose attributes will be converted.
+    Returns:
+        dict: a dictionary containing all serializable attributes of the object
+    """
+    return obj.__dict__
