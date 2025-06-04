e08e37512a1c016c9ba42bd17504306b0dcb1cc9
xl-c111 <10430@holbertonstudents.com>
2025-06-05 00:16:18 +1000

    add comments

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -25,12 +25,30 @@ class Student:
 
         If 'attrs' is not provided or is not a list of strings, all attributes
         of the Student instance will be included in the returned dictionary.
+
+        Args:
+            attrs(list): List of attribute names to include.
+        Returns:
+            dict: Dictionary containing the selected attributes and their values.
         """
         if isinstance(attrs, list) and all(isinstance(attr, str) for
                                            attr in attrs):
             res = {}
+            # loop through each attribute name in attrs
             for attr in attrs:
+                # if self has this attribute. hasattr(obj, name)
                 if hasattr(self, attr):
+                    # get the value and add it to res
                     res[attr] = getattr(self, attr)
             return res
+        # if return self.__dict__ directly, the caller receives a reference of internal dict.
+        # if user modifies this dict, they will actually change the attributes of this dict.
+        # By using .copy() any change made to the returned dict will not affect the original attributes.
         return self.__dict__.copy()
+
+
+"""
+hasattr(obj, name) checks if the obj has an attribute named "name"
+getattr(obj, name) gets the value of the attribute named "name"
+res["first_name"] = "Jack"
+"""
