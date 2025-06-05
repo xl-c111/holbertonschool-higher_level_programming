bc9b54477486e6fb433f56c625cdc35b712146c9
xl-c111 <10430@holbertonstudents.com>
2025-06-06 00:19:07 +1000

    Pickling Custom Classes

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module defines the CustomObject class, which supports pickling-based
+serialization and deserialization of its instances."""
+import pickle
+
+
+class CustomObject:
+    """A custom class that can be serialized to and deserialized from a file
+    using pickle"""
+
+    def __init__(self, name, age, is_student=True):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print("name: {}".format(self.name))
+        print("age: {}".format(self.age))
+        print("is_student: {}".format(self.is_student))
+
+    def serialize(self, filename):
+        with open(filename, "wb") as f:
+            return pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, "rb") as f:
+            return pickle.load(f)
