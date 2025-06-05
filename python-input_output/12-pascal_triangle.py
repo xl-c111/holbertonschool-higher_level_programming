bad3f5077d3d551b383f88415722e5adaa2a924e
xl-c111 <10430@holbertonstudents.com>
2025-06-05 11:53:56 +1000

    Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    res = [[1]]
+
+    for i in range(n - 1):
+        temp = [0] + res[-1] + [0]
+        row = []
+        for j in range(len(res[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
+        res.append(row)
+    return res
--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,6 +2,7 @@
 """This script adds all command-line arguments to a list and saves them in a
 JSON file. If the file exists, previous data will be loaded and extended with
 the new arguments; otherwise, a new file will be created."""
+
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
