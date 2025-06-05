79895225db1ec7e8419b2e6cebb7565cd45f4a0f
xl-c111 <10430@holbertonstudents.com>
2025-06-06 01:06:16 +1000

    add exceptions handling

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -6,10 +6,20 @@ import json
 
 def convert_csv_to_json(csv_filename, json_filename="data.json"):
     """ Convert a CSV file to JSON format."""
-    with open(csv_filename, "r", newline="") as f:
-        reader = csv.DictReader(f)
-        rows = list(reader)
+    try:
+        with open(csv_filename, "r", newline="") as f:
+            reader = csv.DictReader(f)
+            rows = list(reader)
 
-    with open(json_filename, "w") as f:
-        json.dump(rows, f)
-    return True
+        with open(json_filename, "w") as f:
+            json.dump(rows, f)
+        return True
+    except FileNotFoundError:
+        print("{} not found.".format(csv_filename))
+        return False
+    except csv.Error as e:
+        print("Error reading CSV: {e}".format(e))
+        return False
+    except Exception as e:
+        print("Unexpected error: {e}".format(e))
+        return False
