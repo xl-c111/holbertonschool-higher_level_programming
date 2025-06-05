314dcef664ffbf1144f9fa95c8bb099d46b5f60e
xl-c111 <10430@holbertonstudents.com>
2025-06-06 00:57:58 +1000

    change parameter name, add write to json, add return True

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -4,10 +4,12 @@ import csv
 import json
 
 
-def convert_csv_to_json(csv_filename):
+def convert_csv_to_json(csv_filename, json_filename="data.json"):
     """ Convert a CSV file to JSON format."""
-    with open("data.csv", "r", newline="") as f:
-
+    with open(csv_filename, "r", newline="") as f:
         reader = csv.DictReader(f)
-        for row in reader:
-            print(row)
+        rows = list(reader)
+
+    with open(json_filename, "w") as f:
+        json.dump(rows, f)
+    return True
