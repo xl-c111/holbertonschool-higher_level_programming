6bac6c0b2181d5f9dc3d89835f0e85dc23ce0f50
xl-c111 <10430@holbertonstudents.com>
2025-06-05 12:15:39 +1000

    add comments

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,12 +6,28 @@ def pascal_triangle(n):
     """Generate Pascal's triangle up to the nth row (inclusive)"""
     if n <= 0:
         return []
+    # initialize the result list res with the first row of pascal's triangle [1]
     res = [[1]]
 
+    # loop to generate the remaining rows
     for i in range(n - 1):
+
+        # pad the last row with a zero at the beginning and end
         temp = [0] + res[-1] + [0]
+
+        # initialize an enpty list to store pascal's triangle's new row
         row = []
+
+        # j is used to loop the temp list
+        # len(res[-1])+1 is the length of new row we want to generate, which is always one more than the previous row
         for j in range(len(res[-1]) + 1):
             row.append(temp[j] + temp[j + 1])
         res.append(row)
     return res
+
+
+# Workflow foe each subsequent row
+"""
+1, pad the previous row with 0 both at the beginning and end
+2, for each position of new row, sum up each pair of adjecent elements in the padded previous row.
+"""
