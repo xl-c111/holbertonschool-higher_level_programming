991d6910a002f7ac0bf82e20bf613054089897e5
xl-c111 <10430@holbertonstudents.com>
2025-06-10 21:42:21 +1000

    fix '/'n and '/info' handler

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -4,15 +4,23 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
 
 python_dict = {"name": "John", "age": 30, "city": "New York"}
+info_dict = {"version": "1.0",
+             "description": "A simple API built with http.server"}
 
 HTTPServer.allow_reuse_address = True
 
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/data":
-            json_response = json.dumps(python_dict)
+        if self.path == "/":
+            message = "Hello, this is a simple API!"
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(message.encode("utf-8"))
 
+        elif self.path == "/data":
+            json_response = json.dumps(python_dict)
             self.send_response(200)
             self.send_header("Content-Type", "application/json; charset=utf-8")
             self.end_headers()
@@ -21,20 +29,25 @@ class Handler(BaseHTTPRequestHandler):
 
         elif self.path == "/status":
             self.send_response(200)
-            self.send_header("Content-Type", "application/json; charset=utf-8")
+            self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write(b'OK')
+
+        elif self.path == "/info":
+            json_response = json.dumps(info_dict)
+            self.send_response(200)
+            self.send_header("Content-Type", "application/json; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(json_response.encode("utf-8"))
 
         else:
             error = 'json {"error": "404 Not Found"}'
             self.send_response(404)
             self.send_header("Content-Type", "application/json; charset=utf-8")
             self.end_headers()
-
             self.wfile.write(error.encode("utf-8"))
 
 
-PORT = 8000
-with HTTPServer(("", PORT), Handler) as httpd:
-    print("Serve on port 8000...")
+with HTTPServer(("", 8000), Handler) as httpd:
+    print("Serving on port 8000...")
     httpd.serve_forever()
