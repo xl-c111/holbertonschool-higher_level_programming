86347604fb93d32f45d1a7c4082540c1b2fca30c
xl-c111 <10430@holbertonstudents.com>
2025-06-23 15:52:25 +1000

    print full description of the table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
--- Create a table called first_table with id and name
+-- Create a table called first_table with description id and name
 CREATE TABLE IF NOT EXISTS first_table (
   id INT,
   name VARCHAR(256)
---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- Print the description of the first_table
+SHOW CREATE TABLE first_table;
