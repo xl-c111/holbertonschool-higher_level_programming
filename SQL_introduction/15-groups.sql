9e84e87c606944b3d5815466e9bd1de19c09276c
xl-c111 <10430@holbertonstudents.com>
2025-06-23 18:06:05 +1000

    add comments

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -3,3 +3,18 @@ SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
 ORDER BY COUNT(*) DESC;
+
+
+
+"""
+1, SELECT score, COUNT(*) AS number - tell the database to return two columns: 
+   - score: the actual score value in the table
+   - COUNT(*) the number of time each unique appears, it's renamed using 'AS number'
+
+2, GROUP BY score
+   - group rows by their score value, all rows with the same value treated as a group.
+     The COUNT(*) function is then applied to each group
+
+3, ORDER BY COUNT(*) DESC
+   - orders the result in desc order by the number of rows in each group
+"""
