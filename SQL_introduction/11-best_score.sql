1287ef828e978c07641c4392c2ac282269b371cd
xl-c111 <10430@holbertonstudents.com>
2025-06-23 17:05:03 +1000

    fix uppercase error

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- List all records  with a score >= 10 in the table second_table
-SELECT score, name from second_table WHERE score >= 10
+SELECT score, name FROM second_table WHERE score >= 10
 ORDER BY score DESC;
