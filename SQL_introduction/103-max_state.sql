816233608d5a26f1bcdaadd41d25a0a195b28bed
xl-c111 <10430@holbertonstudents.com>
2025-06-24 02:22:28 +1000

    add comments

--- SQL_introduction/103-max_state.sql
+++ SQL_introduction/103-max_state.sql
@@ -3,3 +3,16 @@ SELECT state, MAX(value) as max_temp
 FROM temperatures
 GROUP BY state
 ORDER BY state ASC;
+
+
+
+
+
+"""
+1, import a table dump: 
+cat temperatures.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+
+2, MAX(value): a function returns the highest value
+
+3, ORDER BY state ASC: sort the result by state column in ascending(A to Z) order 
+"""
