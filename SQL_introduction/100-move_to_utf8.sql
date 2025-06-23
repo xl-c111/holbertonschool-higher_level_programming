53c397b70ab51c2d69dddff752c8ba1af94fe677
xl-c111 <10430@holbertonstudents.com>
2025-06-24 01:11:16 +1000

    add comments

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -7,3 +7,17 @@ MODIFY name VARCHAR(256);
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+
+"""
+1, select the target database using USE
+   - Syntax: USE database name
+
+2, remove any column-level charset and collation, keep the column's type and length
+   - Syntax: ATLER table_name
+             MODIFY column_name column_type;
+
+3, set the table's default charset and collation, apply it to relevant columns
+   - Syntax: ATLER table_name
+             CONVERT TO CHARACTER SET ... COLLATE ...
+
+"""
