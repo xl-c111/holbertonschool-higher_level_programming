578fa73a5256f1297bdfd7bf979f05d214dfa7a5
xl-c111 <10430@holbertonstudents.com>
2025-06-23 15:37:53 +1000

    fix unknow database error

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
--- Create a table called first_table with description id and name
-CREATE TABLE IF NOT EXISTS first_table{
-    ID INT,
-    name VARCHAR(256)
-};
+-- Create a table called first_table with id and name
+CREATE TABLE IF NOT EXISTS first_table (
+  id INT,
+  name VARCHAR(256)
+);
