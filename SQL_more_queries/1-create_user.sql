8701d8ca65333539acbe237da3c2c1231e79654e
xl-c111 <10430@holbertonstudents.com>
2025-06-25 00:46:13 +1000

    create a MYSQL server uder

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create the MySQL server user
+-- Create the MySQL server user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON hbtn_0c_0.* TO 'user_0d_1'@'localhost';
