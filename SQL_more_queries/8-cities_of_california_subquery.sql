525e0a7e75d2daa9cd5a0cc3122a1579378d6f4e
xl-c111 <10430@holbertonstudents.com>
2025-06-26 20:50:41 +1000

    correct return column name

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- List all cities of California that can be found in the database hbtn_0d_usa
-SELECT state_id, name
+SELECT id, name
 FROM cities
 WHERE state_id = (
     SELECT id FROM states WHERE name = 'California'
