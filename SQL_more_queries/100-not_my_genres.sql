9a61dbc87591f5698ef9c89c88467403bf13a0f8
xl-c111 <10430@holbertonstudents.com>
2025-06-27 00:42:38 +1000

    list all genres not linked to the show Dexter

---
+++ SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,12 @@
+-- List all genres not linked to the show Dexter
+SELECT DISTINCT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.id NOT IN(
+    SELECT genre_id
+    FROM tv_show_genres
+    JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+    WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY tv_genres.name ASC;
+
+
