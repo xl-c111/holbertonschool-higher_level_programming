12470ead45dd37b32c1d8972ab00fda389361619
xl-c111 <10430@holbertonstudents.com>
2025-06-27 03:25:04 +1000

    add comments

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -2,10 +2,14 @@
 SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
 JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
+-- group the resukt by each by each show ID
 GROUP BY tv_shows.id
 ORDER BY rating DESC;
 
 
 
-Workflow - get sum of ratings per show
-1, 
+-- Workflow - get sum of ratings per show
+-- 1, tv_show_ratings holds score, tv_shows holds title
+-- 2, JOIN both tables by show_id
+-- 3, Use GROUP BY tv_shows.id to group all ratings for the same show together
+-- 4, Use SUMN(rate) to get total rating per group 
