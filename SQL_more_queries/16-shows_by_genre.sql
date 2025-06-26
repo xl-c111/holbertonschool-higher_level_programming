84a5f2dd2131da0700a046881757fd8672cf05d7
xl-c111 <10430@holbertonstudents.com>
2025-06-27 00:11:48 +1000

    add comments

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -4,3 +4,11 @@ FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title ASC, tv_genres.name ASC;
+
+
+
+-- Workflow
+-- 1, Start from the maim table tv_shows, list all shows 
+-- 2, Use tv_show_genres to link shows to genres
+-- 3, Use LEFT JOIN to include shows without genres
+-- 4, Select Column to display: show title, genre name
