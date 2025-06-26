0d708e7d5bbfb8b569415ee364eee79be3359557
xl-c111 <10430@holbertonstudents.com>
2025-06-26 23:31:30 +1000

    fix logic error tv_genres.id = tv_show_genres.genre_id

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,10 @@
 -- List all genres and display the number of shows linked to each
-SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_genres.name AS gerne, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.id
 ORDER BY number_of_shows DESC;
 
--- COUNT(column): count how many in each group(skip empty/null ones)
--- GROUP BY: group by category, put same values together
+
+-- GROUP BY(tv_genres.id): group rows by genre id, put same values together
+-- COUNT(column): count how many shows in each group(skip empty/null ones)
