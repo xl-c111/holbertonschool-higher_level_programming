5e0eca9312382c2f7209512ba0008389e95bb919
xl-c111 <10430@holbertonstudents.com>
2025-06-28 23:17:33 +1000

    try .format()

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -23,9 +23,9 @@ def get_states_by_name(username, password, database, state_name):
             db=database
         )
         cursor = db.cursor()
-        cursor.execute("SELECT * FROM states "      # add a space at the end when concatenating
-                       "WHERE BINARY name = %s "
-                       "ORDER BY states.id ASC", (state_name,))
+        cursor.execute(
+            "SELECT * FROM states WHERE BINARY name = '{}' "
+            "ORDER BY states.id ASC".format(state_name))
         for row in cursor.fetchall():
             print(row)
     except MySQLdb.MySQLError as e:
@@ -51,19 +51,3 @@ if __name__ == "__main__":
     state_name = sys.argv[4]
 
     get_states_by_name(username, password, database, state_name)
-
-
-"""
-Syntax: cursor.execute(query, parameters)
-        - parameters must be a sequence(tuple or list) or a dict
-        - when the SQL uses %s placeholders, the parameters must be a tuple or a list
-        e.g.,
-            query = "SELECT * FROM states WHERE name = %s AND id = %s"
-            params = ("Texas", 3)
-            cursor.execute(query, params)
-        - when the SQL uses %(name)s placeholders, the parameters must be a dict
-        e.g.,
-            query = "SELECT * FROM states WHERE name = %(state_name)s AND id = %(state_id)s"
-            params = {"state_name": "Texas", "state_id": 3}
-            cursor.execute(query, params)
-"""
