32780ab028fa6366908eccbb97b5cd75b34ac876
xl-c111 <10430@holbertonstudents.com>
2025-06-29 02:41:40 +1000

    First state model

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a State class that links to the MySQL table `states`
+using SQLAlchemy ORM."""
+
+
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+
+# Base class for SQLAlchemy models
+Base = declarative_base()
+
+
+class State(Base):
+    """State class mapped to the MySQL table `states`."""
+    __tablename__ = "states"
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
