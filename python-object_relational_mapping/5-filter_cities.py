5d4a48d137dd55d3d2f0c0fe4cae4ba855397770
xl-c111 <10430@holbertonstudents.com>
2025-06-29 00:10:27 +1000

    All cities by state

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Lists all cities of a given state from the database.
+
+Usage:
+    ./5-filter_cities.py <username> <password> <database> 'state_name'
+"""
+import MySQLdb
+import sys
+
+
+def get_cities_by_state(username, password, database, state_name):
+    """
+   Fetches and prints city names of a given state, ordered by city ID.
+    """
+    try:
+        db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+        cursor = db.cursor()
+        cursor.execute(
+            "SELECT cities.id, cities.name, states.name "
+            "FROM cities "
+            "JOIN states ON states.id = cities.state_id "
+            "WHERE BINARY states.name = %s "
+            "ORDER BY cities.id ASC", (state_name,)
+        )
+        cities = [row[1] for row in cursor.fetchall()]
+        print(", ".join(cities))
+    except MySQLdb.MySQLError as e:
+        print("Database Error: {}".format(e))
+    finally:
+        if 'cursor' in locals():
+            cursor.close()
+        if 'db' in locals():
+            db.close()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: ./5-filter_cities.py <username> <password> "
+              "<database> 'state_name'")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    get_cities_by_state(username, password, database, state_name)
