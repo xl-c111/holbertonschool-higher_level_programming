663fcfd609579e5c9b8263f7d5b08a9820aa1956
xl-c111 <10430@holbertonstudents.com>
2025-07-18 23:00:30 +1000

    Convert args to int during for loop by using Number()

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -19,3 +19,26 @@ if (args.length === 0 || args.length === 1) {
   }
   console.log(secondmax);
 }
+
+// Convert each argument to a number during iteration by using Number()
+/*
+const args = process.argv.slice(2);
+
+if (args.length < 2) {
+  console.log(0);
+} else {
+  let max = -Infinity;
+  let secondmax = -Infinity;
+
+  for (let i = 0; i < args.length; i++) {
+    const num = Number(args[i]);
+    if (num > max) {
+      secondmax = max;
+      max = num;
+    } else if (num > secondmax && num < max) {
+      secondmax = num;
+    }
+  }
+  console.log(secondmax);
+}
+*/
