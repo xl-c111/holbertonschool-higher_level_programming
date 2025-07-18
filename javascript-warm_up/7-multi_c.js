a457a1f988eb4ef2c49b72338546e9200d4f3514
xl-c111 <10430@holbertonstudents.com>
2025-07-18 13:51:30 +1000

    Print x times 'C is fun'

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv[2];
+const num = parseInt(args);
+
+if (!isNaN(num)) {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
