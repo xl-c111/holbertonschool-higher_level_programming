d28c3a1df9d334c27de2e2baa3f5486c1d1811a4
xl-c111 <10430@holbertonstudents.com>
2025-07-18 14:20:29 +1000

    Print a square using repeat

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv[2];
+const num = parseInt(args);
+
+if (!isNaN(num)) {
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
+} else {
+  console.log('Missing size');
+}
