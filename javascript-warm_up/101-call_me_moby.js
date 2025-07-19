8b9eb230c679e68882493d9db4214651f9f673da
xl-c111 <10430@holbertonstudents.com>
2025-07-20 03:11:00 +1000

    Update README.md

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,15 +1,20 @@
 #!/usr/bin/node
 
-// exports.callMeMoby = function (x, theFunction) {
-//   for (let i = 0; i < x; i++) {
-//     theFunction();
-//   }
-// };
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};
 
+
+
+// Use while loop to execute x times function 
+/* 
 exports.callMeMoby = function (x, theFunction) {
   let i = 0;
   while (i < x) {
     theFunction();
     i++;
   }
-};
+}
+*/
