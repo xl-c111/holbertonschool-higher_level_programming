2831cbd6e353e4fddee067882d239648eb4dd0e9
xl-c111 <10430@holbertonstudents.com>
2025-07-20 01:33:56 +1000

    Using while loop to execute x times function

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,15 @@
 #!/usr/bin/node
 
+// exports.callMeMoby = function (x, theFunction) {
+//   for (let i = 0; i < x; i++) {
+//     theFunction();
+//   }
+// };
+
 exports.callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++) {
+  let i = 0;
+  while (i < x) {
     theFunction();
+    i++;
   }
 };
