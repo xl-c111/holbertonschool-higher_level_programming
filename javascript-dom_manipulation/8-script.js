a8a223d4267fa455b8bc0805f83749d9a6f004d9
xl-c111 <10430@holbertonstudents.com>
2025-07-25 15:25:14 +1000

    update 8-script.js

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,29 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-  fetch('http://localhost:3000/api/hello?lang=fr')
+  fetch('https://hellosalut.stefanbohacek.com/?lang=fr')
     .then((response) => response.json())
     .then((data) => {
-      const helloDiv = document.getElementById('hello');
-      if (data.hello) {
-        helloDiv.textContent = data.hello;
-      } else if (data.error) {
-        helloDiv.textContent = data.error;
-      } else {
-        helloDiv.textContent = 'Unknown response';
-      }
+      const hello = document.getElementById('hello');
+      hello.textContent = data.hello;
     })
-    .catch((error) => {
-      console.error('Fetch error:', error);
-      document.getElementById('hello').textContent = 'Request failed';
-    });
+    .catch((error) => console.error('Error:', error));
 });
-
-
-
-
-// fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
-//   .then((response) => response.json())
-//   .then((data) => {
-//     document.getElementById('hello').textContent = data.hello;
-//   })
-//   .catch((error) => console.error('Error:', error));
-
