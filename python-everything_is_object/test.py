411eb306accf057df47053b61959b1cd5c6709ff
xl-c111 <10430@holbertonstudents.com>
2025-07-28 11:40:38 +1000

    List incrementation and assignation

--- python-everything_is_object/test.py
+++ python-everything_is_object/test.py
@@ -1,7 +1,8 @@
-l1 = [1, 2, 3]
+def assign_value(n, v):
+    n = v
 
-l2 = l1
-l1 = l1 + [4]
-print(l2)
-print(id(l1))
-print(id(l2))
+
+l1 = [1, 2, 3]
+l2 = [4, 5, 6]
+assign_value(l1, l2)
+print(l1)
